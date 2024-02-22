bf526b7262f3d7ac31a297a6ddc60bd141497219
Payam Zahedi <pzahedi1995@gmail.com>
2024-02-22 09:34:58 +0100

    format the scripts

--- installer.sh
+++ installer.sh
@@ -3,7 +3,6 @@
 # This script installs snapp_installer on your embedded device.
 # It has been tested on Raspberry Pi 4 with Raspbian OS 64-bit.
 
-
 # exit when any command fails
 set -e
 
@@ -20,51 +19,49 @@ main() {
         echo "The $snapp_home directory is already available."
         echo
     fi
-    
+
     cd $snapp_home
-    
+
     curl -f -o snapp_installer https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/snapp_installer
     curl -f -o kiosk.sh https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/kiosk.sh
     curl -f -o config.ini https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/config.ini
-    
+
     echo "add nececerry file permissions to downloaded files"
     echo
-    
+
     # update snapp_installer  file permissions to be executable for all users
     chmod a+x snapp_installer
-    
+
     # update kiosk.sh file permissions to be writable for current user and group
     chmod ug+w kiosk.sh
-    
+
     if ! check_snapp_installed; then
         add_snapp_to_path
         export PATH="$PATH:$snapp_home"
     fi
-    
+
     echo "snapp_installer installed successfully."
     echo
     echo "snapp_installer path:"
     which snapp_installer
-    
+
     echo
     echo "Type snapp_installer in your terminal"
 }
 
-
-add_snapp_to_path(){
+add_snapp_to_path() {
     if grep -q "export PATH=\"\$PATH:$snapp_home\"" "$user_home/.bashrc"; then
         echo "we have snapp_installer as env in ~/.bashrc."
     else
         echo "adding snapp_installer to PATH as env in ~/.bashrc."
-        echo "export PATH=\"\$PATH:$snapp_home\"" >> $user_home/.bashrc
+        echo "export PATH=\"\$PATH:$snapp_home\"" >>$user_home/.bashrc
         source $user_home/.bashrc
     fi
 }
 
-check_snapp_installed(){
+check_snapp_installed() {
     which snapp_installer >/dev/null 2>&1
     return $?
 }
-
 
 main "$@"
--- kiosk.sh
+++ kiosk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Do not change this file. 
+# Do not change this file.
 # It is a template file and will be used by snapp_installer to run the app in kiosk mode.
 
 sleep 2
@@ -16,9 +16,7 @@ echo "Run the flutter app"
 # get the executable app path from user and replace it with the path below
 # placeholder
 
-
 sleep 3
 
 DISPLAY=:0 xdotool key alt+F11
 echo "Run key alt+F11"
-
