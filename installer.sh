3838c72e37b23399968876177ec95e4a6792833c
Payam Zahedi <pzahedi1995@gmail.com>
2024-06-12 12:41:20 +0200

    feat: update installer.sh to use command folder now

--- installer.sh
+++ installer.sh
@@ -3,37 +3,60 @@
 # This script installs snapp_installer on your embedded device.
 # It has been tested on Raspberry Pi 4 with Raspbian OS 64-bit.
 
-# exit when any command fails
+# Exit when any command fails
 set -e
 
 user_home=$(eval echo ~${SUDO_USER})
-snapp_home=$user_home/snapp_installer/bin
+snapp_home="$user_home/snapp_installer/bin"
+temp_dir=$(mktemp -d)
+
+cleanup() {
+    echo "Cleaning up temporary directory: $temp_dir"
+    rm -rf "$temp_dir"
+}
+
+trap cleanup EXIT
 
 main() {
-    # create a folder in user home /embedded
+    # Create a folder in user home /snapp_installer/bin
     if [ ! -d "$snapp_home" ]; then
         echo "Creating directory $snapp_home."
         echo
-        mkdir -p $snapp_home
+        mkdir -p "$snapp_home"
     else
         echo "The $snapp_home directory is already available."
         echo
     fi
 
-    cd $snapp_home
+    cd "$temp_dir"
 
-    curl -f -o snapp_installer https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/snapp_installer
-    curl -f -o kiosk.sh https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/kiosk.sh
-    curl -f -o config.ini https://raw.githubusercontent.com/Snapp-Embedded/snapp_installer/main/config.ini
+    # Clone the repository into the temporary directory
+    echo "Cloning the repository..."
+    git clone --depth 1 https://github.com/Snapp-Embedded/snapp_installer.git
 
-    echo "add nececerry file permissions to downloaded files"
+    cd snapp_installer
+
+    # Enable sparse checkout
+    git sparse-checkout init --cone
+
+    # Specify the folders/files to checkout
+    git sparse-checkout set command snapp_installer kiosk.sh config.ini
+
+    # Copy the necessary files and folders to the snapp_home directory
+    echo "Copying files and folders to $snapp_home..."
+    cp -r command "$snapp_home/"
+    cp snapp_installer "$snapp_home/"
+    cp kiosk.sh "$snapp_home/"
+    cp config.ini "$snapp_home/"
+
+    echo "Adding necessary file permissions to downloaded files"
     echo
 
-    # update snapp_installer  file permissions to be executable for all users
-    chmod a+x snapp_installer
+    # Update snapp_installer file permissions to be executable for all users
+    chmod a+x "$snapp_home/snapp_installer"
 
-    # update kiosk.sh file permissions to be writable for current user and group
-    chmod ug+w kiosk.sh
+    # Update kiosk.sh file permissions to be writable for the current user and group
+    chmod ug+w "$snapp_home/kiosk.sh"
 
     if ! check_snapp_installed; then
         add_snapp_to_path
@@ -51,11 +74,11 @@ main() {
 
 add_snapp_to_path() {
     if grep -q "export PATH=\"\$PATH:$snapp_home\"" "$user_home/.bashrc"; then
-        echo "we have snapp_installer as env in ~/.bashrc."
+        echo "snapp_installer is already in PATH in ~/.bashrc."
     else
-        echo "adding snapp_installer to PATH as env in ~/.bashrc."
-        echo "export PATH=\"\$PATH:$snapp_home\"" >>$user_home/.bashrc
-        source $user_home/.bashrc
+        echo "Adding snapp_installer to PATH in ~/.bashrc."
+        echo "export PATH=\"\$PATH:$snapp_home\"" >>"$user_home/.bashrc"
+        source "$user_home/.bashrc"
     fi
 }
 
